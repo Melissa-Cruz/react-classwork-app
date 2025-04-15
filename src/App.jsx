72d75a49275448e244ced66c6952c9527cc07f8b
Melissa Cruz <melissa.e.cruz@gmail.com>
2025-04-14 21:14:10 -0400

    working on the handler functions  of classwork 3 for 3.1

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,19 @@ import todos from "./data/todos";
 console.log("users :>> ", users);
 console.log("todos :>> ", todos);
 
+
 function App() {
+  const items = ["seltzer", "coke zero", "chocolate", "kiwi"];
+  
+
   return (
     <div>
       <h1>Hello React</h1>
       <Users/>
       <Todos/>
+      <h2>Classwork P3</h2>
+      <ShoppingListForm/>
+      <ShoppingList/>
     </div>
   );
 }
@@ -55,5 +62,49 @@ function Todos() {
     </div>
   )
 }
+
+
+function ShoppingListForm (){
+  const handleItemNameChange = (event)=>{
+    console.log(event)
+  };
+
+  const handleFormSubmit = (event)=>{
+    console.log(event)
+  };
+
+  return(
+
+    <div>
+      <form>
+        <h2>Shopping List Form</h2>
+        <label></label>
+        <input
+        type ="text"
+        id="item"
+        name="item"
+        onChange={handleItemNameChange}
+        />
+        <button type="submit" onSubmit={handleFormSubmit}>Submit</button>
+
+      </form>
+    </div>
+  )
+}
+
+function ShoppingList(){
+  return(
+  <ul>
+    {items.map((item)=>(
+      <li>{item}</li>
+
+    ))
+    }
+  </ul>
+  )
+}
+
+
+
 
 export default App;
